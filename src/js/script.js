acdfa9a4d0cec91e82b1519a2a11893dc865f0b5
Rafael T <rt.lucena@outlook.com>
2024-05-17 09:36:54 -0300

    Criando newsletter

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//Contador
 function medidas(){
     const contador1 = document.getElementById("contador")
     let valor1=0;
@@ -9,4 +10,20 @@ function medidas(){
             clearInterval(tempo1)
         }
     })
+}
+
+//Enviar contato
+function enviar_contato(){
+    var email = document.getElementById("E-mail_contato").value
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email != ""){
+        if (regex.test(email)) {
+            alert("Contato enviado com sucesso!! ✅")
+        } else {
+            alert("Contato inválido ❌");
+        }
+    } else {
+        alert("Existem campos não preenchidos")
+    }
 }
